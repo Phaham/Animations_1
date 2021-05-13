28826e20b0a256094fab6be0f77f54732bfebf13
Phaham <phaham17@gmail.com>
2021-05-14 04:57:11 +0530

    button added

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-let counter =mdocument.querySelector('h1');
-let count = 1;
+// let counter =mdocument.querySelector('h1');
+// let count = 1;
 
-setInterval(()=>{
-    count.innerText = count;
-    count++
+// setInterval(()=>{
+//     count.innerText = count;
+//     count++
 
-    // if(count > 5) location.replace('https://indiazakat.com/campaign/2126/')
-},1000)
+//      if(count > 5) location.replace('https://indiazakat.com/campaign/2126/')
+// },1000)
